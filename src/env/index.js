59f5ade0ec2afdc1b19e0cfd485ed961af657c4e
thalfor <thales.fornazari@gmail.com>
2024-11-05 15:51:31 -0300

    creating the Login logic

--- src/env/index.js
+++ src/env/index.js
@@ -3,11 +3,15 @@ require('dotenv/config');
 const { z } = require('zod');
 //
 const envSchema = z.object({
-  USERNAME: z.string(),
+  USERNAMELOGIN: z.string(),
   PASSWORD: z.string(),
+  USER: z.string(),
 });
 
 const env = envSchema.safeParse(process.env).data;
+
+//console.log(env.USERNAMELOGIN);
+//console.log(env.PASSWORD);
 
 module.exports = env;
 //
--- src/main.js
+++ src/main.js
@@ -1,3 +1,58 @@
 //
+//require('dotenv/config');
+const env = require('./env');
+//const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { setTimeout } = require('node:timers/promises');
+
+
+puppeteer.use(StealthPlugin());
+
+const url = 'https://x.com/i/flow/login'
+
+const loginX = async () => {
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();  
+  await page.goto(url);
+  await setTimeout(3000);
+  
+
+  await page.waitForSelector(`xpath/.//input[@name='text']`); 
+  const [inputLoginField] = await page.$$(`xpath/.//input[@name='text']`);
+  await inputLoginField.type(env.USERNAMELOGIN);
+  //await page.screenshot({ path: 'src/usernameFilled.jpg' });
+  await page.keyboard.press('Enter');
+  await setTimeout(2000);
+
+  
+  // need to check if twitter asked me to put my username for security reasons
+  //await page.screenshot({ path: 'src/whereAmI.jpg' });
+  const checkingWhereAmI = await page.$$(`xpath/.//span[text()='Enter your phone number or username']`); 
+  //console.log(checkingWhereAmI.length);
+  
+  if(checkingWhereAmI.length > 0){
+    await page.waitForSelector(`xpath/.//input[@name='text']`); 
+    const [inputUserField] = await page.$$(`xpath/.//input[@name='text']`);
+    await inputUserField.type(env.USER);
+    //await page.screenshot({ path: 'src/checkUserExistsPage.jpg' });
+    await page.keyboard.press('Enter');
+    await setTimeout(2000);
+  }
+
+
+  await page.waitForSelector(`xpath/.//input[@name='password']`); 
+  const [inputPasswordField] = await page.$$(`xpath/.//input[@name='password']`);
+  await inputPasswordField.type(env.PASSWORD);
+  await page.screenshot({ path: 'src/passwordFilled.jpg' });
+  //await page.keyboard.press('Enter');
+  //await setTimeout(2000);
+
+
+  await browser.close();
+}
+
+loginX();
 
 //
