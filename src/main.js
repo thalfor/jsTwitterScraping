a7c69d776fa4a9d68fd054bbebc1d153668f03f0
thalfor <thales.fornazari@gmail.com>
2024-11-06 16:20:34 -0300

    still trying to take texts from tweets

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const url = 'https://x.com/i/flow/login'
 
 const loginX = async () => {
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   let page = await browser.newPage();  
   await page.goto(url);
   await setTimeout(3000);
@@ -29,11 +29,22 @@ const loginX = async () => {
   await page.goto(urlScrap, { waitUntil: 'networkidle2' })
   await setTimeout(3000);
 
-  await page.screenshot({ path: 'src/pageScrap.jpg' });
+  //$x(`//div[@data-testid="tweetText"][0].childNodes[1].childNodes[0].wholeText`)
+  const [tweetData] = await page.$$(`xpath/.//div[@data-testid='tweetText']`);
+  console.log([tweetData]);
+  /*
+  if(tweetData.length > 0) {
+    for (let i = 0; i < tweetData.lenght ; i++) {
+      let tweets = await page.evaluate(e => e.innerText, tweetData[i]);
+      console.log(tweets);
+    }
+  }
+  
+  await setTimeout(2000);
+  */
 
   await browser.close();
 }
 
 loginX();
-
 //
